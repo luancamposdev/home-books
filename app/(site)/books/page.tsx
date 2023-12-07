809bebbfc495d32ca409da71c818b8d62d402032
Luan Campos <luancamposproducoes@gmail.com>
2023-12-07 11:57:59 -0300

    chore: init pages

--- app/(site)/books/page.tsx
+++ app/(site)/books/page.tsx
@@ -1,7 +1,33 @@
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+
 export default function Books() {
   return (
-    <div className="flex flex-col gap-5 w-full p-5">
-      <h1 className="font-bold text-2xl">Livros</h1>
+    <div
+      className="
+        flex 
+        flex-col 
+        gap-5 
+        w-full 
+        px-0  
+        sm:h-screen 
+        sm:overflow-y-scroll 
+        md:overflow-y-auto
+        bg-black
+        pt-2
+      "
+    >
+      <div className="flex flex-col gap-5 w-full p-5 sm:h-screen sm:overflow-y-scroll md:overflow-y-auto rounded bg-white">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-2xl">Livros</h2>
+          <Button
+            size="icon"
+            className="bg-primaryColor-default hover:bg-primaryColor-default"
+          >
+            <Plus />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
