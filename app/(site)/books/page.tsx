1a3377f5b85212017ebf5c9c48b3e1f200f53b36
Luan Campos <luancamposproducoes@gmail.com>
2024-01-20 12:18:48 -0300

    chore: create header pages

--- app/(site)/books/page.tsx
+++ app/(site)/books/page.tsx
@@ -4,14 +4,16 @@ import { Plus } from 'lucide-react'
 export default function Books() {
   return (
     <div className="flex flex-col gap-5 w-full p-5 sm:h-screen sm:overflow-y-scroll md:overflow-y-auto rounded bg-white">
-      <div className="flex justify-between items-center">
-        <h2 className="font-bold text-2xl">Livros</h2>
-        <Button
-          size="icon"
-          className="bg-primaryColor-default hover:bg-primaryColor-default"
-        >
-          <Plus />
-        </Button>
+      <div className="flex flex-col gap-[15px] w-full">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-2xl">Livros</h2>
+          <Button
+            size="icon"
+            className="bg-primaryColor-default hover:bg-primaryColor-default"
+          >
+            <Plus />
+          </Button>
+        </div>
       </div>
     </div>
   )
